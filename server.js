47219b578d2f6d35d03addd4ef3e48737832e79a
Tatia burdett <tatiaburdett@gmail.com>
2021-03-01 15:27:42 -0600

    create /movies endpoint and add genre query

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
+require('dotenv').config()
 const  express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+console.log(process.env.API_TOKEN)
 
-app.use((req, res) => {
-  res.send('Hello, World! Moviedex API')
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Auth')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  
+  next()
 })
+
+function handleGetMovies(req, res) {
+  let response = MOVIEDEX
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  res.json(response)
+}
+
+app.get('/movies', handleGetMovies)
 
 const PORT = 8000
 
